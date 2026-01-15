7a1ee1d8aece1249313d3a3097bca990910fd5e4
Abdul Rafay <abdulrafay@192.168.1.16>
2026-01-15 13:25:17 +0500

    Player Move and Input Validation

--- game_OOP.py
+++ game_OOP.py
@@ -2,6 +2,36 @@ ROWS = 6
 COLS = 7
 
 board = [['.' for _ in range(COLS)] for _ in range(ROWS)]
+def is_valid_location(board, col):
+    return board[ROWS-1][col] == '.'
+
+def get_next_open_row(board, col):
+    for r in range(ROWS):
+        if board[r][col] == '.':
+            return r
+    return None
+
+def update(board, col, player):
+    for i in range(0, ROWS):
+        if board[i][col] == ".":
+            board[i][col] = player
+            return i, col
+    return None, col
+
+def main():
+    turn = 'H'
+    
+    while True:
+        print_board(board)
+        
+        if turn == 'H':
+            col = int(input("Enter column number: "))
+            if is_valid_location(board, col):
+                row, col = update(board, col, turn)
+                turn = 'A'
+        else:
+            # AI will be added later
+            turn = 'H'
 
 def print_board(board):
     for i in range(ROWS - 1, -1, -1):
